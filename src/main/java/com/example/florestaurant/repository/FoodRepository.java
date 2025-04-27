feeb31bd55586b6e69ea8a4d006784225627685f
vohoainam26 <namvohoai26@gmail.com>
2025-04-27 12:34:49 +0700

    Xóa toàn bộ dữ liệu trên nhánh main

--- src/main/java/com/example/florestaurant/repository/FoodRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.florestaurant.repository;
-
-import com.example.florestaurant.model.Food;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface FoodRepository extends JpaRepository<Food, Long> {
-    List<Food> findByActive(String active);
-}
--- src/main/java/com/example/florestaurant/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.florestaurant.repository;
-
-import com.example.florestaurant.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsernameAndPassword(String username, String password);  // Phương thức tìm người dùng theo username và password
-}
